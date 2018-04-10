2f590ddb1a0ee001086d5eb7ba30c70117f162f6
Temik22 <strokov-artem@mail.ru>
2018-04-10 12:47:53 +0300

    some strings of code

--- src/class/Cut.java
+++ src/class/Cut.java
@@ -2,22 +2,29 @@ import java.io.IOException;
 
 public class Cut {
 
-    public static void main(String[] args) throws Exception{
-        if (args[0].equals("cut") && args[1].equals("-c") && args[args.length - 1].contains("-")){
+    private String ofile = null;
+
+    public static void main(String[] args) throws Exception {
+
+        if (args[2].contains("-o")) {
+            ofile = args[3];
+        }
+
+        if (args[0].equals("cut") && args[1].equals("-c") && args[args.length - 1].contains("-")) {
 
         } else {
-            if (args[0].equals("cut") && args[1].equals("-w") && args[args.length - 1].contains("-")){
+            if (args[0].equals("cut") && args[1].equals("-w") && args[args.length - 1].contains("-")) {
 
-            }else throw new IOException();
+            } else throw new IOException();
         }
 
     }
 
-    public static void toCut(int n, boolean flag, String outName){
+    public static void toCut(int n, int k, boolean flag) {
+        if (flag == false) {
 
-    }
+        } else {
 
-    public static void toCut(int n, int k, String outName){
-
+        }
     }
 }
